e3380e647c9d24062f32e1a131187556d243c19f
Jayabal Malaichamy <balan.ind@gmail.com>
2022-12-26 18:48:23 +0530

    folder structure created

---
+++ src/app/_directives/index.ts
@@ -0,0 +1,3 @@
+export * from './pascalcase/pascalcase.directive';
+export * from './mousehoverevent/mousehoverevent.directive';
+export * from './colorchange/color-change.directive';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './shared/components/header/header.component';
-import { FooterComponent } from './shared/components/footer/footer.component';
+import { HeaderComponent } from './_shared/components/header/header.component';
+import { FooterComponent } from './_shared/components/footer/footer.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
-import { HomeBannerComponent } from './components/home-banner/home-banner.component';
-import { WhyChooseUsComponent } from './components/why-choose-us/why-choose-us.component';
+import { HomeBannerComponent } from './_components/home-banner/home-banner.component';
+import { WhyChooseUsComponent } from './_components/why-choose-us/why-choose-us.component';
 import { CarouselModule } from 'ngx-bootstrap/carousel';
 import { FormsModule } from '@angular/forms';
-import { PascalcaseDirective } from './directives/pascalcase/pascalcase.directive';
-import { ColorChangeDirective } from './directives/colorchange/color-change.directive';
-import { MousehovereventDirective } from './directives/mousehoverevent/mousehoverevent.directive';
+import {
+  ColorChangeDirective,
+  PascalcaseDirective,
+  MousehovereventDirective,
+} from './_directives';
 
 @NgModule({
   declarations: [
